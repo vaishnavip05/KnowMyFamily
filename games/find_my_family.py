8e81177210bb7eb1c44ed621c054659bf93c486b
vaishnavip05 <vaishnavip5114@gmail.com>
2025-12-30 15:55:11 +0530

    Add find_my_family_screen function with Streamlit

--- games/find_my_family.py
+++ games/find_my_family.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+def find_my_family_screen(go_to):
+    st.header("🛤️ Find My Family")
+    st.write("Follow the path and find the correct family member.")
+    st.markdown("---")
+
+    st.info("This game will be implemented next.")
+
+    if st.button("⬅ Back to Home"):
+        go_to("home")
