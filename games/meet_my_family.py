02a35bab3bce8bb1ed010d975357c2991b298baa
vaishnavip05 <vaishnavip5114@gmail.com>
2025-12-30 15:56:05 +0530

    Update meet_my_family.py

--- games/meet_my_family.py
+++ games/meet_my_family.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+def meet_my_family_screen(go_to):
+    st.header("👨‍👩‍👧 Meet My Family")
+    st.write("Learn the names and relationships of your family members.")
+    st.markdown("---")
+
+    st.info("This game will be implemented next.")
+
+    if st.button("⬅ Back to Home"):
+        go_to("home")
